1d6d4c18a990c6172227f8ef8853ea8516511e0b
aakashshrestha <aakash.shrestha9977@gmail.com>
2016-09-30 11:01:43 +0545

    Add files via upload
    Program to find out sum of three no.

---
+++ area.c
@@ -0,0 +1,23 @@
+/*WAP a program to ptint sum of three numbers.
+Student Name:aakash shrestha
+Subject:Programming Fundamentals
+Roll no:
+Program:
+Lab no:04
+Date:2016/9/19
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define pi 3.14
+void main(){
+	
+	float radius,area,circumference;
+	printf("Enter radius of circle:");
+	scanf("%f",&radius);
+	area = 2* pi*pow(radius,2);
+	printf("The area of circle is:%f",area);
+	circumference = 2*pi*radius;
+	printf("The circumference of circle is:%f",circumference);
+	getch();
+}
