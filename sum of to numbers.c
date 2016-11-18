0851f1d0f8fcb83a5cae4007ec5b93bdfb43ffb7
aakashshrestha <aakash.shrestha9977@gmail.com>
2016-11-18 10:36:15 +0545

    Add files via upload

---
+++ sum of to numbers.c
@@ -0,0 +1,28 @@
+/*
+Student Name: Aakash Shrestha
+subject: Programing Fundamentals
+Roll: 01
+Program: Write a C program to print sum of two numbers asked from users.
+Lab:01
+Date:27/09/2016
+*/
+
+  #include<stdio.h>
+     int main()
+
+{
+int a,b,sum;
+
+   printf("enter first number:\n");
+   scanf("%d",&a);
+
+   printf("enter second number:\n");
+   scanf("%d",&b);
+
+   sum=a+b;
+
+   printf("sum of two number of %d",sum);
+
+   return 0;
+}
+
