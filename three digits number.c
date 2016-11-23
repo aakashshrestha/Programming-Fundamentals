1735174ea54f48e6e194deac99e1f867b960c152
aakashshrestha <aakash.shrestha9977@gmail.com>
2016-11-23 10:17:37 +0545

    Add files via upload

---
+++ three digits number.c
@@ -0,0 +1,28 @@
+/*
+   Student Name: Aakash Shrestha
+   Subject: Programing Fundamentals
+   Roll: 01
+   Program: Write a C program to find sum of digits of three digits number.
+   Lab:05
+   Date:23/11/2016
+*/
+
+   #include<stdio.h>
+   //include<conio.h>
+  int main()
+{
+   float a,b,c,sum;
+     printf("enter the value of a=\n");
+     scanf("%f",&a);
+
+     printf("enter the value of b=\n");
+     scanf("%f",&b);
+
+     printf("enter the value of c=\n");
+     scanf("%f",&c);
+
+     sum=a+b+c;
+     printf("sum of three given number=%f",sum);
+
+     return 0;
+}
