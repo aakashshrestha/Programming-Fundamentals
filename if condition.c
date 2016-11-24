40b158572669ae10ae9a95210b7fdee197a22398
aakashshrestha <aakash.shrestha9977@gmail.com>
2016-11-24 11:32:53 +0545

    Add files via upload

---
+++ if condition.c
@@ -0,0 +1,34 @@
+/*
+   Student Name: Aakash Shrestha
+   Subject: Programing Fundamentals
+   Roll: 01
+   Program: Write a C pro.
+   Lab:04
+   Date:24/11/2016
+*/
+
+     #include<stdio.h>
+     //#include<conio.h>
+
+   int main()
+
+{
+    int n;
+      printf("enter the value of n\n");
+      scanf("%d",&n);
+
+      if(n>0)
+      {
+      printf("number is positive \n");
+      }
+      else if(n==0)
+      {
+      printf("number is=0\n");
+      }
+      else
+      {
+      printf("number is negative\n");
+      }
+
+      return 0;
+}
