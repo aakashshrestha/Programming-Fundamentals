56c64da99b4455aa1f89321b7525d41d3ca598f9
aakashshrestha <aakash.shrestha9977@gmail.com>
2016-12-14 10:42:01 +0545

    Add files via upload

---
+++ whether number is divisible by 5 & 11 or not.c
@@ -0,0 +1,29 @@
+/*
+Student Name: Aakash Shrestha
+subject: Programing Fundamentals
+Roll: 01
+Program: Write a C program to check whether a number is divisible by 5 & 11 or not.
+Lab:01
+Date:27/09/2016
+*/
+
+  #include<stdio.h>
+  //#include<conio.h>
+      int main()
+{
+        int a;
+           printf("Enter the first number\n");
+           scanf("%d",&a);
+
+            if (a%5==0&&a%11==0)
+           {
+            printf("%d is divisible by 5 & 11\n",a);
+           }
+
+           else
+           {
+            printf("%d is not divisible by 5 & 11\n",a);
+           }
+
+           return 0;
+}
